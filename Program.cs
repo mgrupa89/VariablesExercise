011ec802fa4160409ee6a9a54882a2e97a4cc029
mgrupa89 <69479947+mgrupa89@users.noreply.github.com>
2020-08-24 17:28:57 -0500

    First variable commit.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace variables
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string dogName = "Reinhardt";
+			int dogAge = 3;
+			char firstLetter = 'R'; //single characters NEED single quotes
+			bool likeDogs;
+			double toys = 32;
+			decimal timeOwned = 2.5m;
+
+
+
+
+			Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old. \n" + $"The first letter of his name begins with {firstLetter} and the number of toys he has is {toys}. \n" + $"I have owned him for {timeOwned}. \n" + "Do you like dogs?");
+			Console.Write("> ");
+
+
+			string userInput = (Console.ReadLine().ToLower().Trim());
+			
+
+			
+
+			if (userInput == "no")
+			{
+				likeDogs = false;
+				Console.WriteLine("You Monster!");
+
+			}
+
+
+
+			else if (userInput == "yes")
+
+			{
+				likeDogs = true;
+				Console.WriteLine("Awesome, I like dogs too!");
+
+
+			}
+			else
+			{
+				Console.WriteLine("Please enter a yes or no answer.");
+			}
+		}
+	}
+}
